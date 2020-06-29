56679d8d867b3dcf657e767a6a2d5e498661de07
ayorindeadunse <adunseayorinde@gmail.com>
2020-06-30 00:23:56 +0100

    fixed bug on src/index.js, included vote update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 const Query = require("./resolvers/Query");
-const Mutation = require("./resolvers/Mutation").default;
+const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
 const Subscription = require("./resolvers/Subscription");
